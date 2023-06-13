4bfddb95bf3ce97cefe7b3f4fc3a0abb3d1d0a07
navyavenkat <navyavenkat03@gmail.com>
2023-06-13 08:05:19 -0700

    Initial work is done to save the file structure

---
+++ src/SearchBar.jsx
@@ -0,0 +1,37 @@
+const SearchBar = ({setSearchQuery}) => (
+    <form>
+      <TextField
+        id="search-bar"
+        className="text"
+        onInput={(e) => {
+          setSearchQuery(e.target.value);
+        }}
+        label="Enter a city name"
+        variant="outlined"
+        placeholder="Search..."
+        size="small"
+      />
+      <IconButton type="submit" aria-label="search">
+        <SearchIcon style={{ fill: "blue" }} />
+      </IconButton>
+    </form>
+  );
+  const filterData = (query, data) => {
+    if (!query) {
+      return data;
+    } else {
+      return data.filter((d) => d.toLowerCase().includes(query));
+    }
+  };
+  const data = [
+    "Paris",
+    "London",
+    "New York",
+    "Tokyo",
+    "Berlin",
+    "Buenos Aires",
+    "Cairo",
+    "Canberra",
+    "Rio de Janeiro",
+    "Dublin"
+  ];
